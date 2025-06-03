103ed5534ec52fda5b8b4f3e761bc74aeb1581ab
Brodino <brodino96@gmail.com>
2025-06-03 23:26:34 +0200

    Initial commit

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,26 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+function fish_greeting
+end
+
+zoxide init fish | source
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+alias remove="paru -Rsn --noconfirm"
+
+if status is-interactive
+    set -x ZELLIJ_AUTO_EXIT true
+    set -x ZELLIJ_AUTO_ATTACH true
+
+    if set -q ZELLIJ
+    else
+        zellij
+    end
+end
+
+starship init fish | source
