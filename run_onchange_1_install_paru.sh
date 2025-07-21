606101d8350dabd47032c7c908acd476b2247bd9
Brodino <brodino96@gmail.com>
2025-07-21 16:44:13 +0200

    feat: reorganized

--- run_onchange_1_install_paru.sh
+++ run_onchange_1_install_paru.sh
@@ -1,39 +1,50 @@
 #!/bin/sh
 
+# Packages
 paru -S flatpak --noconfirm --needed
 paru -S topgrade --noconfirm --needed
 
-paru -S rustup --noconfirm --needed
-paru -S nodejs --noconfirm --needed
-
-paru -S git --noconfirm --needed
-paru -S github-cli --noconfirm --needed
+# Console
 paru -S wezterm --noconfirm --needed
-paru -S fzf --noconfirm --needed
+paru -S zellij --noconfirm --needed
 paru -S zoxide --noconfirm --needed
 paru -S starship --noconfirm --needed
+paru -S fzf --noconfirm --needed
 paru -S otf-cascadia-code --noconfirm --needed
 paru -S ttf-cascadia-mono-nerd --noconfirm --needed
+
+# Coding
+paru -S rustup --noconfirm --needed
+paru -S nodejs --noconfirm --needed
+paru -S git --noconfirm --needed
+paru -S github-cli --noconfirm --needed
+
+# Windows
 paru -S ntfs-3g --noconfirm --needed
-paru -S btop --noconfirm --needed
 
-paru -S libdbusmenu-glib --noconfirm --needed # vscode dep
-paru -S icu69-bin --noconfirm --needed # vscode dep
-paru -S visual-studio-code-bin --noconfirm --needed
-
-paru -S google-chrome -launcher --noconfirm --needed
-paru -S spotify-launcher --noconfirm --needed
-
+# Chats
 paru -S telegram-desktop --noconfirm --needed
 paru -S signal-desktop --noconfirm --needed
 paru -S discord --noconfirm --needed
 paru -S zapzap --noconfirm --needed
 
+# Games
 paru -S steam --noconfirm --needed
 paru -S proton-ge-custom-bin --noconfirm --needed
 paru -S bottles --noconfirm --needed
 paru -S wine-cachyos --noconfirm --needed
 paru -S curseforge --noconfirm --needed
 paru -S heroic-games-launcher-bin --noconfirm --needed
-
 paru -S gpu-screen-recorder-ui --noconfirm --needed
+
+# Tools
+paru -S libdbusmenu-glib --noconfirm --needed # vscode dep
+paru -S icu69-bin --noconfirm --needed # vscode dep
+paru -S visual-studio-code-bin --noconfirm --needed
+paru -S google-chrome --noconfirm --needed
+paru -S spotify-launcher --noconfirm --needed
+paru -S btop --noconfirm --needed
+
+# OS Theme
+paru -S cachyos-emerald-kde-theme-git --noconfirm --needed
+paru -S papirus-icon-theme --noconfirm --needed
