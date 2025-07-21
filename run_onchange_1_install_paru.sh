618025434ddcc13f7ec55d25aca58ab4f3f55bbf
Brodino <brodino96@gmail.com>
2025-07-21 18:20:29 +0200

    feat: changed whatsapp client

--- run_onchange_1_install_paru.sh
+++ run_onchange_1_install_paru.sh
@@ -27,7 +27,7 @@ paru -S ntfs-3g --noconfirm --needed
 paru -S telegram-desktop --noconfirm --needed
 paru -S signal-desktop --noconfirm --needed
 paru -S discord --noconfirm --needed
-paru -S zapzap --noconfirm --needed
+paru -S whatsapp-for-linux --noconfirm --needed
 
 # Games
 paru -S steam --noconfirm --needed
