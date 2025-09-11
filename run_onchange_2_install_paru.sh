ac68912188f267cc82dcda19fc0e3b8f49f55dd8
Brodino <brodino96@gmail.com>
2025-09-11 14:02:15 +0200

    refactor(install): changed order

--- run_onchange_2_install_paru.sh
+++ run_onchange_2_install_paru.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Packages
+# Package managment
 paru -S flatpak --noconfirm --needed
 paru -S topgrade --noconfirm --needed
 
-# Console
+# Terminal
 paru -S wezterm --noconfirm --needed
 paru -S zellij --noconfirm --needed
 paru -S zoxide --noconfirm --needed
@@ -14,13 +14,16 @@ paru -S otf-cascadia-code --noconfirm --needed
 paru -S ttf-cascadia-mono-nerd --noconfirm --needed
 
 # Coding
+paru -S libdbusmenu-glib --noconfirm --needed # vscode dependency
+paru -S icu69-bin --noconfirm --needed # vscode dependency
+paru -S visual-studio-code-bin --noconfirm --needed
 paru -S rustup --noconfirm --needed
 paru -S nodejs --noconfirm --needed
 paru -S bun --noconfirm --needed
 paru -S git --noconfirm --needed
 paru -S github-cli --noconfirm --needed
 
-# Windows
+# WindowsFS
 paru -S ntfs-3g --noconfirm --needed
 
 # Chats
@@ -34,20 +37,16 @@ paru -S proton-ge-custom-bin --noconfirm --needed
 paru -S bottles --noconfirm --needed
 paru -S wine-cachyos --noconfirm --needed
 paru -S heroic-games-launcher-bin --noconfirm --needed
+paru -S gpu-screen-recorder-ui --noconfirm --needed
 # Minecraft
 paru -S modrinth-app-bin --noconfirm --needed
 paru -S curseforge --noconfirm --needed
 
 # Tools
-paru -S libdbusmenu-glib --noconfirm --needed # vscode dependency
-paru -S icu69-bin --noconfirm --needed # vscode dependency
-paru -S visual-studio-code-bin --noconfirm --needed
-paru -S firefox --noconfirm --needed
 paru -S spotify-launcher --noconfirm --needed
 paru -S btop --noconfirm --needed
 paru -S opendeck --noconfirm --needed
 paru -S rofi --noconfirm --needed
-paru -S gpu-screen-recorder-ui --noconfirm --needed
 
 # OS Theme
 paru -S cachyos-emerald-kde-theme-git --noconfirm --needed
