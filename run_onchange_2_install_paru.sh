00270d99f7deb2022fc5ccccb6bee4270a135c13
Brodino <brodino96@gmail.com>
2025-09-11 18:40:28 +0200

    feat(os): added catpuccin theme

--- run_onchange_2_install_paru.sh
+++ run_onchange_2_install_paru.sh
@@ -52,6 +52,7 @@ paru -S rofi --noconfirm --needed
 paru -S cachyos-emerald-kde-theme-git --noconfirm --needed
 paru -S papirus-icon-theme --noconfirm --needed
 paru -S kwin-effect-rounded-corners --noconfirm --needed
+paru -S catppuccin-plasma-colorscheme-mocha --noconfirm --needed
 
 # Obs
 paru -S obs-vkcapture --noconfirm --needed
