58b60b04509822b5325bb38363a6f70b84ea3188
Brodino <brodino96@gmail.com>
2025-11-16 14:40:10 +0100

    feat(install_paru): add rnote installation

--- run_onchange_2_install_paru.sh
+++ run_onchange_2_install_paru.sh
@@ -51,6 +51,7 @@ paru -S btop --noconfirm --needed
 paru -S opendeck --noconfirm --needed
 paru -S rofi --noconfirm --needed
 paru -S google-chrome --noconfirm --needed
+paru -S rnote --noconfirm --needed
 # Video
 paru -S vlc --noconfirm --needed
 paru -S vlc-plugins-all --noconfirm --needed
