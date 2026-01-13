98826770f832d23c752ba083280adac7f857d5d5
Brodino <brodino96@gmail.com>
2026-01-13 13:44:09 +0100

    feat: replaced rofi with vicinae inside the install script

--- run_onchange_3_install_paru.sh
+++ run_onchange_3_install_paru.sh
@@ -49,7 +49,7 @@ paru -S spotify-launcher --noconfirm --needed
 paru -S rocm-smi-lib --noconfirm --needed
 paru -S btop --noconfirm --needed
 paru -S opendeck --noconfirm --needed
-paru -S rofi --noconfirm --needed
+paru -S vicinae-bin --noconfirm --needed
 paru -S google-chrome --noconfirm --needed
 paru -S rnote --noconfirm --needed
 # Video
